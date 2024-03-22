f1433ed7170c16c0d56d7fed667f3c54654165af
JustCheburek <artemiznakhdki@gmail.com>
2024-03-22 15:59:11 +1000

    maxSize

--- src/ui/components/maxSize.tsx
+++ src/ui/components/maxSize.tsx
@@ -1,22 +1,27 @@
 // React
-import type {PropsWithChildren} from "react";
+import type {ComponentPropsWithoutRef} from "react";
 
 // Стили
 import styles from "./styles/maxSize.module.scss";
 
-type MaxSize = {
-	className?: string
-	sideNav?: boolean
-	width?: number
+declare module 'csstype' {
+	interface Properties {
+		'--max-width'?: string
+	}
 }
 
-export function MaxSize({children, sideNav=false, className = "", width = 1200}: PropsWithChildren<MaxSize>) {
+type MaxSize = {
+	width?: number
+	sideNav?: boolean
+}
+
+export function MaxSize({children, sideNav=false, className = "", width = 1200, ...props}: ComponentPropsWithoutRef<"div"> & MaxSize) {
 	if (sideNav && width === 1200) {
 		width = 1300
 	}
 
 	return (
-			<div className={`${styles.max_size} ${sideNav ? styles.sideNav : ""} ${className}`} style={{"--max-width": `${width}px`}}>
+			<div className={`${styles.max_size} ${sideNav ? styles.sideNav : ""} ${className}`} style={{"--max-width": `${width}px`}} {...props}>
 				{children}
 			</div>
 	)
