1ad025b294ff20c569499ad0e20b2ce13e2203b5
JustCheburek <artemiznakhdki@gmail.com>
2024-04-28 10:40:29 +1000

    metrika

--- app/script.tsx
+++ app/script.tsx
@@ -1,36 +1,43 @@
 "use client";
 
+import {useEffect} from "react";
+
 export function Metrika() {
-	return (
-			<script dangerouslySetInnerHTML={{
-				__html:
-						function metrica() {
-							setTimeout(() => {
-								(function (m, e, t, r, i, k, a) {
-									m[i] = m[i] || function () {
-										(m[i].a = m[i].a || []).push(arguments)
-									};
-									m[i].l = 1 * new Date();
-									for (let j = 0; j < document.scripts.length; j++) {
-										if (document.scripts[j].src === r) {
-											return;
-										}
-									}
-									// noinspection CommaExpressionJS
-									k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-								})
-								(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+	useEffect(() => {
+		window.addEventListener('scroll', metrica, {once: true})
+		return () => window.removeEventListener('scroll', metrica)
+	})
 
-								ym(91732193, "init", {
-									clickmap: true,
-									trackLinks: true,
-									accurateTrackBounce: true,
-									webvisor: true
-								});
-							}, 1000)
-						}
+	function metrica() {
+		setTimeout(() => {
+			(function (m, e, t, r, i, k, a) {
+				// @ts-ignore
+				m[i] = m[i] || function () {
+					// @ts-ignore
+					(m[i].a = m[i].a || []).push(arguments)
+				};
+				// @ts-ignore
+				m[i].l = 1 * new Date();
+				for (let j = 0; j < document.scripts.length; j++) {
+					if (document.scripts[j].src === r) {
+						return;
+					}
+				}
+				// @ts-ignore
+				// noinspection CommaExpressionJS
+				k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+			})
+			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-				// window.addEventListener('scroll', metrica, {once: true})
-			}}/>
-	)
+			// @ts-ignore
+			ym(91732193, "init", {
+				clickmap: true,
+				trackLinks: true,
+				accurateTrackBounce: true,
+				webvisor: true
+			});
+		}, 1000)
+	}
+
+	return
 }
