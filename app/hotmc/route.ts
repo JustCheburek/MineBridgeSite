d3a240085127241ab06134dd6f5278cedb096309
JustCheburek <artemiznakhdki@gmail.com>
2024-06-30 13:45:43 +1000

    hotmc

---
+++ app/hotmc/route.ts
@@ -0,0 +1,24 @@
+import {NextRequest, NextResponse} from "next/server";
+import {sha1} from "js-sha1";
+
+export async function POST(request: NextRequest) {
+	const res = await request.json();
+
+	if (!res.nick || !res.time || !res.sign) {
+		return new NextResponse(`Не переданы необходимые данные`, {
+			status: 422
+		});
+	}
+
+	if (res.sign !== sha1(res.nick + res.time + process.env.HOTMC_SECRET)) {
+		return new NextResponse(`Переданные данные не прошли проверку`, {
+			status: 409
+		});
+	}
+
+	console.log(`Всё ок ${res.nick} получает мостики`)
+
+	return new NextResponse(`ok`, {
+		status: 200
+	});
+}
