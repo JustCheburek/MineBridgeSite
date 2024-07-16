7beecfd8db920cfb8fc34788c14bc69a548f729e
JustCheburek <artemiznakhdki@gmail.com>
2024-07-16 16:11:29 +1000

    form

--- app/user/[name]/components/form.tsx
+++ app/user/[name]/components/form.tsx
@@ -2,12 +2,14 @@ import type {User} from "lucia";
 import {userModel} from "@server/models";
 import {FormClient} from "./formClient";
 import {revalidateTag} from "next/cache";
+import {redirect} from "next/navigation";
 
 // мс * с * мин * ч * д
 const threeDays = 1000 * 60 * 60 * 24 * 3
 
 export function FormBox({author}: { author: User | null }) {
-	if (!author || author?.form === true) return
+	if (!author) return
+	if (author?.form === true) return
 
 	const time = new Date().getTime() - new Date(author.createdAt).getTime()
 	if (time < threeDays) return
@@ -16,8 +18,9 @@ export function FormBox({author}: { author: User | null }) {
 		"use server"
 
 		await userModel.findByIdAndUpdate(author!._id, {form: true})
-
 		revalidateTag("userLike")
+
+		redirect(`https://forms.yandex.ru/u/6638c564f47e731e00d9e6e1/`)
 	}
 
 	return <FormClient Func={Func}/>
--- app/user/[name]/components/formClient.tsx
+++ app/user/[name]/components/formClient.tsx
@@ -7,18 +7,8 @@ type FormClient = {
 }
 
 export function FormClient({Func}: FormClient) {
-	function ClientFunc() {
-		window.open(
-				"https://forms.yandex.ru/u/6638c564f47e731e00d9e6e1/",
-				"_blank",
-				"popup,width=750,height=600"
-		)
-
-		Func()
-	}
-
 	return (
-			<form action={() => ClientFunc}>
+			<form action={() => Func}>
 				<button type="submit" className={styles.form_box}>
 					<h3 className="unic_color center_text">
 						❤️️ Пройдите опрос и оцените сервер ❤️️
