4e9ca4ae81c2a06e1581a107aa23099922799556
JustCheburek <artemiznakhdki@gmail.com>
2024-07-23 15:54:18 +1000

    hotmc ready

--- app/hotmc/route.ts
+++ app/hotmc/route.ts
@@ -1,24 +1,26 @@
 import {NextRequest, NextResponse} from "next/server";
-// import {sha1} from "js-sha1";
+import {sha1} from "js-sha1";
 
 export async function POST(request: NextRequest) {
 	const res = await request.formData()
-	console.log(res)
 
-	/*if (!res.nick || !res.time || !res.sign) {
-		return new NextResponse(`Не переданы необходимые данные`, {
+	const nick = res.get("nick") as string | null
+	const time = res.get("time") as string | null
+	const sign = res.get("sign") as string | null
+
+	if (!nick || !time || !sign) {
+		return new NextResponse("Не переданы необходимые данные", {
 			status: 422
 		});
 	}
 
-	if (res.sign !== sha1(res.nick + res.time + process.env.HOTMC_SECRET)) {
-		return new NextResponse(`Переданные данные не прошли проверку`, {
+	if (sign !== sha1(nick + time + process.env.HOTMC_SECRET)) {
+		return new NextResponse("Переданные данные не прошли проверку", {
 			status: 409
 		});
-	}*/
+	}
 
-
-	return new NextResponse(`ok`, {
+	return new NextResponse("ok", {
 		status: 200
 	});
 }
