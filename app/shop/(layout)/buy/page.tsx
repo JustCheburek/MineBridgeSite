1eb42d3f2688766ad68d7f66c64931d5751d9342
JustCheburek <artemiznakhdki@gmail.com>
2024-09-06 16:47:41 +1000

    sbp size

--- app/shop/(layout)/buy/page.tsx
+++ app/shop/(layout)/buy/page.tsx
@@ -13,105 +13,105 @@ import {OnThisPage, OnThisPageHeading, OnThisPageLink} from "@components/sideNav
 import {H1} from "@components/h1";
 
 export const metadata: Metadata = {
-	title: "Покупка | MineBridge",
-	description: "Покупка мостиков с помощью СБП. 1₽ = 1 мостик. Подержите нас донатиком, пж!",
+    title: "Покупка | MineBridge",
+    description: "Покупка мостиков с помощью СБП. 1₽ = 1 мостик. Подержите нас донатиком, пж!",
 };
 
 export default async function Component() {
-	const {user} = await validate(cookies().get(lucia.sessionCookieName)?.value)
+    const {user} = await validate(cookies().get(lucia.sessionCookieName)?.value)
 
-	if (!user) {
-		return permanentRedirect("/auth")
-	}
+    if (!user) {
+        return permanentRedirect("/auth")
+    }
 
-	return (<>
-		<div className="center_text">
-			<H1>
-				Покупка
-			</H1>
-			<h3 id="mostiki">
-				1 ₽ = 1 <MostikiSvg/>
-			</h3>
+    return (<>
+        <div className="center_text">
+            <H1>
+                Покупка
+            </H1>
+            <h3 id="mostiki">
+                1 ₽ = 1 <MostikiSvg/>
+            </h3>
 
-			<h4>
-				Для покупки позовите {" "}
-				<Link
-						href="https://discord.gg/f95V9Rezqy"
-						className="unic_color medium-font" target="_blank"
-				>
-					админа
-				</Link>
-			</h4>
+            <h4>
+                Для покупки позовите {" "}
+                <Link
+                    href="https://discord.gg/f95V9Rezqy"
+                    className="unic_color medium-font" target="_blank"
+                >
+                    админа
+                </Link>
+            </h4>
 
-			<PBox id="sbp">
-				<PTitle>
-					<h2 className="unic_color">СБП</h2>
-				</PTitle>
-				<PText className="center_text">
-					<h4 className="all_select green_color">
-						СберБанк
-					</h4>
-					<h4 className="all_select">
-						8 914 344 8578
-					</h4>
-				</PText>
-			</PBox>
+            <PBox id="sbp">
+                <PTitle>
+                    <h2 className="unic_color">СБП</h2>
+                </PTitle>
+                <PText className="center_text">
+                    <h4 className="all_select green_color">
+                        СберБанк
+                    </h4>
+                    <h4 className="all_select">
+                        8 914 344 8578
+                    </h4>
+                </PText>
+            </PBox>
 
-			<PBox id="sber">
-				<PTitle>
-					<Link
-							href="https://www.sberbank.com/sms/pbpn?requisiteNumber=79143448578"
-							target="_blank" className="unic_color"
-					>
-						<h2>СБЕР</h2>
-					</Link>
-				</PTitle>
-				<PText>
-					<Link
-							href="https://www.sberbank.com/sms/pbpn?requisiteNumber=79143448578"
-							target="_blank"
-					>
-						<SBPSvg/>
-					</Link>
-				</PText>
-			</PBox>
+            <PBox id="sber">
+                <PTitle>
+                    <Link
+                        href="https://www.sberbank.com/sms/pbpn?requisiteNumber=79143448578"
+                        target="_blank" className="unic_color"
+                    >
+                        <h2>СБЕР</h2>
+                    </Link>
+                </PTitle>
+                <PText>
+                    <Link
+                        href="https://www.sberbank.com/sms/pbpn?requisiteNumber=79143448578"
+                        target="_blank"
+                    >
+                        <SBPSvg size="15rem"/>
+                    </Link>
+                </PText>
+            </PBox>
 
-			<PBox id="donationAlerts">
-				<PTitle>
-					<Link
-							href="https://www.donationalerts.com/r/kawa11fox"
-							target="_blank" className="unic_color"
-					>
-						<h2>Donation Alerts</h2>
-					</Link>
-				</PTitle>
-				<PText>
-					<Link
-							href="https://www.donationalerts.com/r/kawa11fox"
-							target="_blank"
-					>
-						Комиссию оплачиваете Вы
-					</Link>
-				</PText>
-			</PBox>
-		</div>
+            <PBox id="donationAlerts">
+                <PTitle>
+                    <Link
+                        href="https://www.donationalerts.com/r/kawa11fox"
+                        target="_blank" className="unic_color"
+                    >
+                        <h2>Donation Alerts</h2>
+                    </Link>
+                </PTitle>
+                <PText>
+                    <Link
+                        href="https://www.donationalerts.com/r/kawa11fox"
+                        target="_blank"
+                    >
+                        Комиссию оплачиваете Вы
+                    </Link>
+                </PText>
+            </PBox>
+        </div>
 
-		<OnThisPage>
-			<OnThisPageHeading>
-				Содержание
-			</OnThisPageHeading>
-			<OnThisPageLink href="#mostiki">
-				Цена
-			</OnThisPageLink>
-			<OnThisPageLink href="#sbp">
-				СБП
-			</OnThisPageLink>
-			<OnThisPageLink href="#sber">
-				Сбер
-			</OnThisPageLink>
-			<OnThisPageLink href="#donationAlerts">
-				Donation Alerts
-			</OnThisPageLink>
-		</OnThisPage>
-	</>)
+        <OnThisPage>
+            <OnThisPageHeading>
+                Содержание
+            </OnThisPageHeading>
+            <OnThisPageLink href="#mostiki">
+                Цена
+            </OnThisPageLink>
+            <OnThisPageLink href="#sbp">
+                СБП
+            </OnThisPageLink>
+            <OnThisPageLink href="#sber">
+                Сбер
+            </OnThisPageLink>
+            <OnThisPageLink href="#donationAlerts">
+                Donation Alerts
+            </OnThisPageLink>
+        </OnThisPage>
+    </>)
 }
