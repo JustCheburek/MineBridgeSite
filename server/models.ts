de019c6324f6bb6fea81eaa636b5d1cf43eccb51
JustCheburek <artemiznakhdki@gmail.com>
2024-09-14 22:30:57 +1000

    dependencies fixes?

--- server/models.ts
+++ server/models.ts
@@ -8,8 +8,8 @@ import {Connect} from "./connect";
 
 Connect()
 
-export const userModel = (models["User"] || getModelForClass(User)) as ReturnModelType<typeof User>
-export const sessionModel = (models["Session"] || getModelForClass(Session)) as ReturnModelType<typeof Session>
-export const caseModel = (models["Case"] || getModelForClass(Case)) as ReturnModelType<typeof Case>
-export const dropModel = (models["Drop"] || getModelForClass(Drop)) as ReturnModelType<typeof Drop>
-export const seasonModel = (models["Season"] || getModelForClass(Season)) as ReturnModelType<typeof Season>
+export const userModel = (models?.User || getModelForClass(User)) as ReturnModelType<typeof User>
+export const sessionModel = (models?.Session || getModelForClass(Session)) as ReturnModelType<typeof Session>
+export const caseModel = (models?.Case || getModelForClass(Case)) as ReturnModelType<typeof Case>
+export const dropModel = (models?.Drop || getModelForClass(Drop)) as ReturnModelType<typeof Drop>
+export const seasonModel = (models?.Season || getModelForClass(Season)) as ReturnModelType<typeof Season>
