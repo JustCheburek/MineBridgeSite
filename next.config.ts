9c9a2d4aa5d4b816134bc984979a324af882155b
JustCheburek <artemiznakhdki@gmail.com>
2024-10-28 15:28:11 +1000

    update next js 15

---
+++ next.config.ts
@@ -0,0 +1,30 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      }
+    ]
+  },
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api"],
+  },
+  webpack: (config) => {
+    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
+    config.experiments = {
+      topLevelAwait: true,
+      layers: true
+    };
+    return config;
+  }
+};
+
+export default nextConfig;
