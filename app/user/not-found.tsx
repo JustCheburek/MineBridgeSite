9114299b148f64f22fe1a17791e8435031e24aaf
JustCheburek <artemiznakhdki@gmail.com>
2024-10-29 11:47:26 +1000

    fix cookies in notfound

--- app/user/not-found.tsx
+++ app/user/not-found.tsx
@@ -2,20 +2,21 @@ import {MaxSize} from "@components/maxSize";
 import {Url} from "@components/button";
 import {validate} from "@services/validate";
 import {redirect} from "next/navigation";
-import {cookies} from "next/headers";
 import {lucia} from "@server/lucia";
 import {H1} from "@components/h1";
+import {cookies} from "next/headers";
 
 export default async function NotFound() {
-	const {user} = await validate(cookiesStore.get(lucia.sessionCookieName)?.value)
+	const cookiesStore = await cookies()
+    const {user} = await validate(cookiesStore.get(lucia.sessionCookieName)?.value)
 
-	if (user) redirect(`/user/${user.name}`)
+    if (user) redirect(`/user/${user.name}`)
 
-	return (
-		<MaxSize className="center_text">
-				<H1>А, кто это?</H1>
-				<h3>Игрок не найден</h3>
-				<Url href="/users">Посмотреть игроков</Url>
-			</MaxSize>
-	)
+    return (
+        <MaxSize className="center_text">
+            <H1>А, кто это?</H1>
+            <h3>Игрок не найден</h3>
+            <Url href="/users">Посмотреть игроков</Url>
+        </MaxSize>
+    )
 }
