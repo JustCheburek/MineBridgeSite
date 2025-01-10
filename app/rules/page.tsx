5927f87532cc10bb7d64e854ff747663efcba85b
JustCheburek <artemiznakhdki@gmail.com>
2025-01-10 21:27:24 +1000

    rules mobile

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -34,8 +34,8 @@ export default function Rules() {
                         </strong>{" "}
                         <small>
                             ({LASTRULESUPDATE.toLocaleDateString("ru-RU", {
-                            dateStyle: "long"
-                        })})
+                                dateStyle: "long"
+                            })})
                         </small>
                     </time>
                 </p>
