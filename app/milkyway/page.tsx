ae2e51fdc704d50a81379939a1b7adcb690b90e4
JustCheburek <artemiznakhdki@gmail.com>
2025-01-11 22:50:46 +1000

    milkyway

--- app/milkyway/page.tsx
+++ app/milkyway/page.tsx
@@ -3,7 +3,60 @@ import {MaxSize} from "@components/maxSize";
 import {validate} from "@services/validate";
 import {FormLink} from "@components/formBox";
 import styles from "./milkyway.module.scss"
-import {StarSvg, TriangleSvg} from "@ui/SVGS";
+
+declare module 'csstype' {
+    interface Properties {
+        '--_size'?: string
+        '--_x'?: string
+        '--_y'?: string
+        '--_long'?: string
+        '--_angle'?: string
+    }
+}
+
+const size = 4.5
+
+const Path = ({rating, now, x, y = 0, next = 0}: {
+    rating: number,
+    now: number,
+    x: number,
+    y?: number,
+    next?: number
+}) => {
+    let long = 0
+    let angle = 0
+
+    if (next !== 0) {
+        const width = Math.abs(x) + Math.abs(next) + size
+        const height = (y + size) * 2
+        long = Math.sqrt(width ** 2 + height ** 2) / 2
+        angle = Math.atan2(
+            height,
+            next > x
+                ? width
+                : -width
+        )
+    }
+
+    return (
+        <div
+            className={styles.container}
+            style={{
+                '--_x': `${x}rem`,
+                '--_y': `${y}rem`,
+                '--_long': `${long}rem`,
+                '--_angle': `${angle}rad`
+            }}
+        >
+            <div className={styles.box}>
+                <h3 className={styles.rating}>
+                    {rating}
+                </h3>
+                <div className={`${styles.line} ${now >= rating ? styles.unic : ""}`}/>
+            </div>
+        </div>
+    )
+}
 
 export default async function MilkyWay() {
     const {user: author} = await validate()
@@ -14,34 +67,22 @@ export default async function MilkyWay() {
                 <H1>Млечный путь</H1>
 
                 <FormLink href="/auth">
-                    Сначала зарегистрируйся
+                    Войти в акк
                 </FormLink>
             </MaxSize>
         )
     }
 
     return (
-        <MaxSize className="center_text">
+        <MaxSize className="center_text" style={{"--_size": `${size}rem`}}>
             <H1>
                 Млечный путь
             </H1>
 
-            <div className={styles.progress_box}>
-                <progress
-                    value={150}
-                    max={300}
-                    className={styles.progress}
-                />
-
-                <div className={styles.rating_box}>
-                    <TriangleSvg className={styles.triangle} size="1.2em"/>
-                    <span className={styles.line}/>
-
-                    <h3 className={styles.rating}>
-                        {author.rating} <StarSvg/>
-                    </h3>
-                </div>
-            </div>
+            <Path now={author.rating} rating={25} x={-15} y={2} next={20}/>
+            <Path now={author.rating} rating={50} x={20} y={5} next={-40}/>
+            <Path now={author.rating} rating={75} x={-40} y={10} next={20}/>
+            <Path now={author.rating} rating={100} x={20}/>
         </MaxSize>
     )
 }
