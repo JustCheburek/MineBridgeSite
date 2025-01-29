7c22f7967ff28121f400862dc331cb9bf1e7e7c4
JustCheburek <artemiznakhdki@gmail.com>
2025-01-29 21:57:46 +1000

    remove пользовательское соглашение

--- ui/components/footer.tsx
+++ ui/components/footer.tsx
@@ -3,7 +3,6 @@ import "./styles/footer.scss";
 
 // Компоненты
 import {Urls} from "./urls";
-import Link from "next/link";
 
 export function Footer() {
     const YEAR = new Date().getFullYear()
@@ -17,11 +16,6 @@ export function Footer() {
                 <p className="center_text">
                     &#169; MineBridge 2022-{YEAR}
                 </p>
-                <Link className="unic_color medium-font center_text"
-                   href="https://docs.google.com/document/d/1EwOizUalpazLDMlw6yQijd5oVN39IF473J0TD0n47gE/edit?usp=sharing"
-                   target="_blank">
-                    Пользовательское соглашение
-                </Link>
                 <small>
                     <p>
                         Not an official Minecraft product. We are in no way affiliated with or endorsed by Mojang Synergies AB, Microsoft Corporation or other rightsholders.
