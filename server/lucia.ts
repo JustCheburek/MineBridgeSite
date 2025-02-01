2902b21f2809ac0bc1e0e18e03ae9a2d61aeeb5b
JustCheburek <artemiznakhdki@gmail.com>
2025-02-01 22:36:51 +1000

    ru url

--- server/lucia.ts
+++ server/lucia.ts
@@ -24,4 +24,4 @@ declare module "lucia" {
 
 export const discord = new Discord(process.env.DISCORD_CLIENT_ID!, process.env.DISCORD_CLIENT_SECRET!, `${process.env.NEXT_PUBLIC_RU_URL}/auth/discord/callback`);
 export const google = new Google(process.env.GOOGLE_CLIENT_ID!, process.env.GOOGLE_CLIENT_SECRET!, `${process.env.NEXT_PUBLIC_PUNYCODE_URL}/auth/google/callback`)
-export const twitch = new Twitch(process.env.TWITCH_CLIENT_ID!, process.env.TWITCH_CLIENT_SECRET!, `${process.env.NEXT_PUBLIC_PUNYCODE_URL}/auth/twitch/callback`)
+export const twitch = new Twitch(process.env.TWITCH_CLIENT_ID!, process.env.TWITCH_CLIENT_SECRET!, `${process.env.NEXT_PUBLIC_RU_URL}/auth/twitch/callback`)
