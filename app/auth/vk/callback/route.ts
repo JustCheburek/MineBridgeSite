45d445588d07fabd1d35d6cd7f6e55e774c2108c
JustCheburek <artemiznakhdki@gmail.com>
2025-02-01 20:06:09 +1000

    full error

--- app/auth/vk/callback/route.ts
+++ app/auth/vk/callback/route.ts
@@ -1,11 +1,7 @@
-import {vk, lucia} from "@server/lucia";
+import {vk} from "@server/lucia";
 import {cookies} from "next/headers";
-import {generateId, User} from "lucia";
-import type {GUser, VKUser} from "@/types/user";
-import {OAuth2RequestError} from "arctic";
-import {userModel} from "@server/models";
+import type {VKUser} from "@/types/user";
 import {NextRequest, NextResponse} from "next/server";
-import {validate} from "@services/validate";
 import axios from "axios";
 
 
@@ -26,84 +22,84 @@ export async function GET(request: NextRequest) {
         )
     }
 
-    try {
-        // Получение пользователя
-        const tokens = await vk.validateAuthorizationCode(code);
+    /*try {*/
+    // Получение пользователя
+    const tokens = await vk.validateAuthorizationCode(code);
 
-        console.log(`tokens: ${JSON.stringify(tokens)}`)
+    console.log(`tokens: ${JSON.stringify(tokens)}`)
 
-        const accessToken = tokens.accessToken();
+    const accessToken = tokens.accessToken();
 
-        console.log(`accessToken: ${accessToken}`)
+    console.log(`accessToken: ${accessToken}`)
 
-        const vkUser = await axios.get<VKUser>("https://api.vk.com/method/users.get", {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
+    const vkUser = await axios.get<VKUser>("https://api.vk.com/method/users.get", {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    }).then(r => r.data).catch(console.error);
+
+    console.log(`vkUser: ${JSON.stringify(vkUser)}`)
+
+    /*if (!vkUser || !vkUser.email) {
+        return new NextResponse("Проверьте вк аккаунт", {status: 400})
+    }
+
+
+
+    const userData = {
+        _id: generateId(15),
+        name: cookiesStore.get("name")?.value,
+        vkId: gUser.sub,
+        email: gUser.email,
+        photo: gUser.picture
+    } as User
+
+    const {user} = await validate()
+
+    if (user) {
+        await userModel.findByIdAndUpdate(
+            user._id,
+            {
+                email: userData.email,
+                vkId: userData.vkId
             }
-        }).then(r => r.data).catch(console.error);
+        )
+    } else {
+        let candidate = await userModel.findOneAndUpdate(
+            {
+                $or: [
+                    {vkId: userData.vkId},
+                    {vkId: Number(userData.vkId)},
+                    {email: userData.email}
+                ]
+            },
+            {
+                email: userData.email,
+                vkId: userData.vkId,
+            },
+            {
+                new: true
+            }
+        )
 
-        console.log(`vkUser: ${JSON.stringify(vkUser)}`)
-
-        /*if (!vkUser || !vkUser.email) {
-            return new NextResponse("Проверьте вк аккаунт", {status: 400})
+        if (!candidate) {
+            candidate = await userModel.create(userData)
         }
 
+        const session = await lucia.createSession(candidate?._id || userData._id, {});
+        const sessionCookie = lucia.createSessionCookie(session.id);
+        cookiesStore.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+    }*/
 
-
-        const userData = {
-            _id: generateId(15),
-            name: cookiesStore.get("name")?.value,
-            vkId: gUser.sub,
-            email: gUser.email,
-            photo: gUser.picture
-        } as User
-
-        const {user} = await validate()
-
-        if (user) {
-            await userModel.findByIdAndUpdate(
-                user._id,
-                {
-                    email: userData.email,
-                    vkId: userData.vkId
-                }
-            )
-        } else {
-            let candidate = await userModel.findOneAndUpdate(
-                {
-                    $or: [
-                        {vkId: userData.vkId},
-                        {vkId: Number(userData.vkId)},
-                        {email: userData.email}
-                    ]
-                },
-                {
-                    email: userData.email,
-                    vkId: userData.vkId,
-                },
-                {
-                    new: true
-                }
-            )
-
-            if (!candidate) {
-                candidate = await userModel.create(userData)
-            }
-
-            const session = await lucia.createSession(candidate?._id || userData._id, {});
-            const sessionCookie = lucia.createSessionCookie(session.id);
-            cookiesStore.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-        }*/
-
-        return new NextResponse(
-            `Всё успешно`,
-            {
-                /*status: 302,
-                headers: {
-                    Location: `/user/${userData.name}`
-                }*/
-            });
-    } catch (e) {
+    return new NextResponse(
+        `Всё успешно`,
+        {
+            /*status: 302,
+            headers: {
+                Location: `/user/${userData.name}`
+            }*/
+        });
+    /*} catch (e) {
         if (e instanceof OAuth2RequestError && e.message === "bad_verification_code") {
             return new NextResponse(`Ошибка в коде регистрации ${e}`, {
                 status: 400
@@ -112,5 +108,5 @@ export async function GET(request: NextRequest) {
         return new NextResponse(`Ошибка: ${e}`, {
             status: 500
         });
-    }
+    }*/
 }
