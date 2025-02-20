998b70d2fdcbf31021830f012bc885a7e0f64045
JustCheburek <artemiznakhdki@gmail.com>
2025-02-20 22:05:00 +1000

    create season fix

--- services/seasons/create.ts
+++ services/seasons/create.ts
@@ -1,10 +1,9 @@
+"use server";
 import {Season} from "@/types/season";
 import {seasonModel} from "@server/models";
 import {revalidateTag} from "next/cache";
 
 export async function createSeason(season: Season) {
-    "use server";
-
     await seasonModel.create({
         number: season.number,
         startAt: new Date(season.startAt),
