940ab64dd0c12b38f3bd86c5a2887848cb977731
JustCheburek <artemiznakhdki@gmail.com>
2025-02-23 19:19:58 +1000

    model fix?

--- server/models.ts
+++ server/models.ts
@@ -8,8 +8,8 @@ import {models} from "mongoose";
 
 Connect()
 
-export const userModel = (models?.User || getModelForClass(User)) as ReturnModelType<typeof User>
-export const sessionModel = (models?.Session || getModelForClass(Session)) as ReturnModelType<typeof Session>
-export const caseModel = (models?.Case || getModelForClass(Case)) as ReturnModelType<typeof Case>
-export const dropModel = (models?.Drop || getModelForClass(Drop)) as ReturnModelType<typeof Drop>
-export const seasonModel = (models?.Season || getModelForClass(Season)) as ReturnModelType<typeof Season>
+export const userModel = (models?.User || models?.Users || getModelForClass(User)) as ReturnModelType<typeof User>
+export const sessionModel = (models?.Session || models?.Sessions || getModelForClass(Session)) as ReturnModelType<typeof Session>
+export const caseModel = (models?.Case || models?.Cases || getModelForClass(Case)) as ReturnModelType<typeof Case>
+export const dropModel = (models?.Drop || models?.Drops || getModelForClass(Drop)) as ReturnModelType<typeof Drop>
+export const seasonModel = (models?.Season || models?.Seasons || getModelForClass(Season)) as ReturnModelType<typeof Season>
