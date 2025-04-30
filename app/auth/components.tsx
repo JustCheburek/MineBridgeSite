d15f1fb3aea65bb6f4523c41534d21d612991ea2
JustCheburek <artemiznakhdki@gmail.com>
2025-04-30 20:03:25 +1000

    toLowerCase

--- app/auth/components.tsx
+++ app/auth/components.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {DefaultFormBox, FormGroup, FormInput, FormLabel, FormLink} from "@components/formBox";
+import {DefaultFormBox, FormA, FormGroup, FormInput, FormLabel} from "@components/formBox";
 import {InputNameCheck} from "@components/formInputs";
 import {DiscordSvg, GoogleSvg, TwitchSvg} from "@ui/SVGS";
 import {useState} from "react";
@@ -53,9 +53,9 @@ export function AuthForm({savedName}: { savedName?: string }) {
                 </FormLabel>
             </FormGroup>
 
-            <FormLink href={`/auth/${provider}?name=${name}`} prefetch={false}>
+            <FormA href={`/auth/${provider}?name=${name}`}>
                 Дальше
-            </FormLink>
+            </FormA>
         </DefaultFormBox>
     )
 }
