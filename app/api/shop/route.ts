72a0ed3acd252bbf66fd4750b194dd2b2e5f09cb
JustCheburek <artemiznakhdki@gmail.com>
2025-05-11 11:28:26 +1000

    email

--- app/api/shop/route.ts
+++ app/api/shop/route.ts
@@ -2,6 +2,10 @@ import {NextRequest, NextResponse} from 'next/server'
 import crypto from 'crypto'
 import {PaymentPost} from "@/types/payment";
 import {userModel} from "@db/models";
+import {MostikiEmail} from "@email/mostiki";
+import {Resend} from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
     const payment = await request.json() as PaymentPost
@@ -23,10 +27,23 @@ export async function POST(request: NextRequest) {
         return NextResponse.json({status: 'OK'})
     }*/
 
+    const mostiki = payment.cost
+
     const user = await userModel.findOneAndUpdate(
         {email: payment.email},
-        {$inc: {mostiki: payment.cost}}
+        {$inc: {mostiki}}
     )
+
+    if (user?.notifications?.mostiki) {
+        await resend.emails.send({
+            from: 'Майнбридж <mostiki@m-br.ru>',
+            to: user.email,
+            subject: 'Изменения в мостиках на MineBridge',
+            react: MostikiEmail(
+                {name: user.name, mostiki, allMostiki: user.mostiki + mostiki}
+            )
+        })
+    }
 
     console.log(user)
 
