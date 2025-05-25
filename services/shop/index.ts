fd087546388d9c1aa9cc66a7ff0b816aa43fe52a
JustCheburek <artemiznakhdki@gmail.com>
2025-05-25 20:58:03 +1000

    import fix

--- services/shop/index.ts
+++ services/shop/index.ts
@@ -4,131 +4,165 @@ import {unstable_cache as cache} from "next/cache";
 import {caseModel, dropModel} from "@db/models";
 import {Case, Drop, Item, RarityType} from "@/types/case";
 import {idOrName} from "@/types/idOrName";
-import {notFound} from "next/navigation";
+
+export const getCaseLocal = cache(
+    async (
+        param: idOrName,
+        Cases: Case[]
+    ) => {
+        const Case = Cases.find(({name, _id}) =>
+            name === param.name || String(_id) === String(param._id)
+        )
+
+        if (!Case) {
+            console.error(`Case не найден: ${JSON.stringify(param)}`)
+        }
+
+        return Case
+    },
+    ["case", "shop", "all"],
+    {revalidate: 3600, tags: ["case", "shop", "all"]}
+)
 
 export const getCase = cache(
-    async ({name, _id}: idOrName): Promise<Case> => {
-        let caseM
+    async (
+        param: idOrName
+    ) => {
+        const Case: Case | null = JSON.parse(JSON.stringify(
+            await caseModel.findOne(
+                param,
+                {},
+                {
+                    lean: true
+                })
+        ))
 
-        if (name) {
-            caseM = await caseModel.findOne({name}, {}, {lean: true})
-        } else {
-            caseM = await caseModel.findById(_id, {}, {lean: true})
+        if (!Case) {
+            throw new Error(`Case не найден: ${JSON.stringify(param)}`)
         }
 
-        const case_: Case | null = JSON.parse(JSON.stringify(caseM))
-
-        if (!case_) {
-            console.error(`Case не найден: ${name ?? _id}`)
-            notFound()
-        }
-
-        return case_
+        return Case
     },
-    ["case", "all"],
-    {revalidate: 300, tags: ["case", "all"]}
+    ["case", "shop", "all"],
+    {revalidate: 3600, tags: ["case", "shop", "all"]}
 )
 
 export const getCases = cache(
-    async () => {
-        const cases: Case[] = JSON.parse(JSON.stringify(
-            await caseModel.find({}, {}, {lean: true})
-        ))
+    async (): Promise<Case[]> => JSON.parse(JSON.stringify(
+        await caseModel.find(
+            {},
+            {},
+            {
+                lean: true
+            })
+    )),
+    ["cases", "shop", "all"],
+    {revalidate: 3600, tags: ["cases", "shop", "all"]}
+)
 
-        return cases
+export const getDropLocal = cache(
+    async (
+        param: idOrName,
+        Drops: Drop[]
+    ) => {
+        const Drop = Drops.find(({name, _id}) =>
+            name === param.name || String(_id) === String(param._id)
+        )
+
+        if (!Drop) {
+            console.error(`Drop не найден: ${JSON.stringify(param)}`)
+        }
+
+        return Drop
     },
-    ["cases", "all"],
-    {revalidate: 300, tags: ["cases", "all"]}
+    ["drop", "shop", "all"],
+    {revalidate: 3600, tags: ["drop", "shop", "all"]}
 )
 
 export const getDrop = cache(
-    async ({name, _id}: idOrName): Promise<Drop> => {
-        let dropM
+    async (
+        param: idOrName
+    ) => {
+        const Drop: Drop | null = JSON.parse(JSON.stringify(
+            await dropModel.findOne(
+                param,
+                {},
+                {
+                    lean: true
+                }
+            )
+        ))
 
-        if (name) {
-            dropM = await dropModel.findOne({name}, {}, {lean: true})
-        } else {
-            dropM = await dropModel.findById(_id, {}, {lean: true})
+        if (!Drop) {
+            throw new Error(`Drop не найден: ${JSON.stringify(param)}`)
         }
 
-        const drop: Drop | null = JSON.parse(JSON.stringify(dropM))
-
-        if (!drop) {
-            console.error(`Drop не найден: ${name ?? _id}`)
-            notFound()
-        }
-
-        return drop
+        return Drop
     },
-    ["drop", "all"],
-    {revalidate: 300, tags: ["drop", "all"]}
+    ["drop", "shop", "all"],
+    {revalidate: 3600, tags: ["drop", "shop", "all"]}
 )
 
 export const getDrops = cache(
     async () => {
         const drops: Drop[] = JSON.parse(JSON.stringify(
-            await dropModel.find({}, {}, {lean: true})
+            await dropModel.find(
+                {},
+                {},
+                {
+                    lean: true,
+                    sort: {price: -1}
+                }
+            )
         ))
 
         return drops
     },
-    ["drops", "all"],
-    {revalidate: 300, tags: ["drops", "all"]}
-)
-
-export const getDropLocal = cache(
-    async (drop: Drop): Promise<Drop> => {
-        const drops = await getDrops()
-        const dropLocal = drops.find(({_id}) => String(_id) === String(drop._id))
-
-        if (!dropLocal) {
-            console.error(`Drop не найден: ${drop._id}`)
-            notFound()
-        }
-
-        return dropLocal
-    },
-    ["drop", "all"],
-    {revalidate: 300, tags: ["drop", "all"]}
-)
-
-export const getCaseLocal = cache(
-    async (case_: Case): Promise<Case> => {
-        const cases = await getCases()
-        const caseLocal = cases.find(({_id}) => String(_id) === String(case_._id))
-
-        if (!caseLocal) {
-            console.error(`Case не найден: ${case_._id}`)
-            notFound()
-        }
-
-        return caseLocal
-    },
-    ["case", "all"],
-    {revalidate: 300, tags: ["case", "all"]}
-)
-
-export const getItem = cache(
-    async (drop: Drop, rarity: RarityType, item: Item): Promise<Item> => {
-        const dropLocal = await getDropLocal(drop)
-        const itemLocal = dropLocal.items[rarity].find(({name}) => name === item.name)
-
-        if (!itemLocal) {
-            console.error(`Item не найден: ${item.name}`)
-            notFound()
-        }
-
-        return itemLocal
-    },
-    ["item", "all"],
-    {revalidate: 300, tags: ["item", "all"]}
+    ["drops", "shop", "all"],
+    {revalidate: 3600, tags: ["drops", "shop", "all"]}
 )
 
 export const getItems = cache(
-    async (drop: Drop, rarity: RarityType): Promise<Item[]> => {
-        const dropLocal = await getDropLocal(drop)
-        return dropLocal.items[rarity]
+    async (
+        rarity: RarityType,
+        DropItem?: Drop
+    ) => {
+        if (!DropItem) return []
+        let Items = DropItem?.drop
+        if (Items?.length === 0) {
+            Items = DropItem[rarity]
+        }
+
+        if (Items?.length === 0 || !Items) {
+            console.error(`Items не найден`)
+        }
+
+        return Items || []
     },
-    ["items", "all"],
-    {revalidate: 300, tags: ["items", "all"]}
-) 
+    ["items", "shop", "all"],
+    {revalidate: 3600, tags: ["items", "shop", "all"]}
+)
+
+export const getItem = cache(
+    async (
+        param: idOrName,
+        Items: Item[]
+    ) => {
+        if (Items.length === 0) {
+            return Items[0]
+        }
+
+        const Item = Items.find(({_id, name}) =>
+            String(_id) === String(param._id) ||
+            name === param.name
+        )
+
+        if (!Item) {
+            console.error(`Item не найден: ${JSON.stringify(param)}`)
+        }
+
+        return Item
+    },
+    ["item", "shop", "all"],
+    {revalidate: 3600, tags: ["item", "shop", "all"]}
+)
