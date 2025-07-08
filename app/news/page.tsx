cc33211ca57e81824efcf88c1359cefad29a0405
JustCheburek <artemiznakhdki@gmail.com>
2025-07-08 13:19:26 +1000

    fredimine in news

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from 'next'
 import { revalidateTag } from 'next/cache'
 import { H1 } from '@components/h1'
-import { SeasonBox } from '@components/post'
+import { PBox, PTag, PTags, PText, PTitle, SeasonBox } from '@components/post'
 import { NewsInfiniteList } from './components'
 import { getSeasons } from '@/services/seasons'
 import { Suspense } from 'react'
 import { Skeleton } from '@components/skeleton'
+import { Img, ImgBox } from '@components/img'
+import { CheckLink } from '@/ui/components/checkLink'
+import Link from 'next/link'
+import { TG_URL } from '@/const'
 
 export const revalidate = 3600
 
@@ -14,6 +18,7 @@ export const metadata: Metadata = {
   description: 'Важнейшие новости, избираемые из телеграма майнбриджа. Здесь интересно!',
 }
 
+// todo: деление на сезоны
 export default async function News() {
   const seasons = await getSeasons()
 
@@ -28,6 +33,25 @@ export default async function News() {
       >
         Новости
       </H1>
+
+      <PBox>
+        <Link href={TG_URL}>
+            <ImgBox type="post">
+              <Img src='/news/tg.jpg' alt='Новости' />
+            </ImgBox>
+          </Link>
+        <PTitle>
+          <h3>🌶 Новости сервера 🌶</h3>
+        </PTitle>
+        <PText className="whitespace-pre-line [&_*]:leading-[1.7em] [&>p]:my-0.5 [&>.blockquote]:my-0.5">
+          <p>
+            Следить за новостями можно в нашем <Link href={TG_URL} className='text-unic font-bold'>телеграм-канале</Link> 📲
+          </p>
+          <p>
+            Подписывайся, чтобы не пропустить <Link href={TG_URL} className='text-unic font-bold'>важные новости</Link> 💬
+          </p>
+        </PText>
+      </PBox>
 
       {/* {seasons.map(season =>
                 <div key={season.number}>
--- ui/components/notFound.tsx
+++ ui/components/notFound.tsx
@@ -4,7 +4,6 @@ import type { PropsWithChildren } from 'react'
 // Компоненты
 import { Url } from './button'
 import { SearchSvg } from '@ui/SVGS'
-import { cn } from '@/lib/utils'
 
 export function NotFound({
   children,
@@ -12,9 +11,9 @@ export function NotFound({
   href,
 }: PropsWithChildren<{ buttonText: string; href: string }>) {
   return (
-    <section className='my-[25px] grid gap-6 text-center max-[1200px]:hidden'>
+    <section className='my-[25px] grid gap-6'>
       <SearchSvg className='text-unic mx-auto size-[8rem]' />
-      <h3 className='text-unic'>Не нашли нужную информацию?</h3>
+      <h3 className='text-unic mx-auto'>Не нашли нужную информацию?</h3>
 
       <p>
         Нажмите <code>CTRL + F</code> на windows или <code>CMD + F</code> на мак
